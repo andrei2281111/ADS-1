542993f7280db8b174e45dae0ed17f1d9b7aa26b
Daniil1254 <62605512+Daniil1254@users.noreply.github.com>
2020-03-26 23:28:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,40 +1,17 @@
 int cbinsearch(int *arr, int size, int value)
 {
-  int center = size/2;
-  int k = 2;
-  int amountElements = 0;
-  
-    for(int i = 2; (1 < (size / i)) && (arr[center + size/k] != value) ; i += 2)
-    {
-    if (arr[center + size/k] > value) 
-    {
-     k += 2; 
-     center = center - size/k;
-    }
-    
-    if (arr[center + size/k] < value) 
-    {
-     k += 2; 
-     center = center + size/k;
-    }
-    }
-    
-    if (arr[center + size/k] == value) 
-    {
-     amountElements++;
-     
-     while (arr[center + size/k + 1] == value)
-      {
-       amountElements++;
-       size/k += 1;      
-      }
-      
-      while (arr[center - size/k - 1] == value)
-      {
-       amountElements++;
-       size/k -= 1;
-      }
-      
-      return amountElements;
-    }
+    int left = 0, right = size - 1, center , amountElements = 0, c = 0;
+    while (left <= right)
+        {
+            center = (left + right) / 2; 
+            if (arr[center] == value) c     = center; 
+            if (arr[center] > value)  right = center - 1; 
+            else                      left  = center + 1; 
+        }
+        while (arr[c] == value)
+        {
+            amountElements++; 
+            c--; 
+        }
+    return amountElements;
 }
