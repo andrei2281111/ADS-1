ad2b0014b24199bd6574c3f7797c86fe0bec3661
dizzki <62598699+dizzki@users.noreply.github.com>
2020-03-27 00:42:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,28 @@
 int cbinsearch(int *arr, int size, int value)
 {
-    int n=0;
-    for(int i=0;i<size;i++)
-    {
-        if(arr[i]==value)
-        n++;
-    }
-    return n;
+    int n=0,k=0;
+    int left = 0;
+    int right = size-1;
+    while (left <= right)
+      {
+            int mid = (left + right) / 2;
+            if (value == arr[mid])
+                {
+                  n++;
+                  k=mid;
+                }
+            if (arr[mid] < value)
+                {left = mid + 1;}
+            else {right = mid - 1;}
+      }
+ if (n>0)
+  {
+      n=0;
+      while(arr[k]==value)
+      {
+          n++;
+          k++;
+      }
+  } 
+return n;
 }
