be416c9dbae45c1a355d740b6990f58ef41ea6de
NeklyudovaYulya <62639484+NeklyudovaYulya@users.noreply.github.com>
2020-03-29 14:21:34 +0300

    Create alg.cpp

---
+++ src/alg.cpp
@@ -0,0 +1,23 @@
+int cbinsearch (int *arr, int size, int value)
+{
+int a = 0;
+int b = size-1;
+int c = 0;
+int d = 0;
+     while (a <= b)
+     {
+        int middle = (a+b)/2;
+          if (arr[middle] == value) 
+            d=middle;
+          if (arr[middle] < value)
+            a = middle + 1;
+          else
+            b = middle - 1;
+     }
+     while (arr[d] == value)
+     {
+         c++;
+         d--;
+     }
+     return c;
+}
