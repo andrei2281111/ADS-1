05cbc5aaa01c9ab7c0719d0f130c3fe22ffeb0d0
IrinaMoiseeva <62604038+IrinaMoiseeva@users.noreply.github.com>
2020-04-10 15:16:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,37 @@
 int cbinsearch(int *arr, int size, int value)
 {
-int l = 0, s = size, midle = 0, a = 0;
-while (l < s)
-{
-midle = (l + s - 1) / 2;
-if (arr[midle] > value)
-s = midle - 1;
-else if (arr[midle] < value)
-l = midle + 1;
-else if (arr[midle] == value)
-a++;
+int res=0, midle, low = 0, high = size-1, move;
+
+while (low<=high) {
+midle = (low+high)/2;
+
+if (value == arr[midle]) {
+res++;
+move=midle;
+while (move>0) {
+if (arr[move-1]!=value)
 break;
-}
-if (a == 1)
-{
-for (int i = 0; i < midle; i++)
-{
-if (arr[i] == value)
-a++;
-}
-for (int i = s - 1; i > midle; i--)
-{
-if (arr[i] == value)
-a++;
+else {
+res++;
+move--;
 }
 }
-return a;
+move=midle;
+while (move<size-1) {
+if (arr[move+1]!=value)
+break;
+else {
+res++;
+move++;
+}
+}
+return res;
+}
+else if (value<arr[midle])
+high = midle -1;
+else
+low = midle + 1;
+}
+
+return res;
 }
