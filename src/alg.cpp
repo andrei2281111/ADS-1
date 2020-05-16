2c11db7efca784307a4da2d9e9949973212fe297
TumashukMV <65454334+TumashukMV@users.noreply.github.com>
2020-05-16 17:37:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,23 +1,27 @@
-int cbinsearch(int *arr, int size, int value){
-  int first = 0;
-  int last = size - 1;
-  int search = 0;
-  while(first <= last){
-  int middle = first + (last-first)/2;
-    if(arr[middle] == value){
-    search++;
-      for(int i = 1; arr[middle + i] == value; i++){
-      search++;
-      }
-      for(int j = 1; arr[middle - j] == value; j++){
-        search++;
-      }
-      return search;
-    }
-    if(arr[middle] < value){
-      first = middle + 1;
-      else last = middle - 1;
-    }
-  }
-return search;
+int cbinsearch(int *arr, int size, int value) {
+
+	int mid = 0;
+	int pos = -1;
+	int left = 0;
+	int right = size-1;
+	while (left <= right) {
+		mid = (left + right) / 2;
+		if (arr[mid] == value) pos = mid;
+		if (arr[mid] < value) left = mid + 1;
+		else right = mid - 1;
+	}
+	mid = 0;
+	int pos1 = -1;
+	left = 0;
+	right = size - 1;
+	while (left <= right) {
+		mid = (left + right) / 2;
+		if (arr[mid] == value) pos1 = mid;
+
+			if (arr[mid] > value) right = mid - 1;
+			else left = mid + 1;
+
+	}
+	if ((pos1 == -1) && (pos == -1)) return 0; else
+	return pos1-pos+1;
 }
